4bbb6d2b2ccbc0f152db5c50b3d03ce8db35d1fa
Vincent <vincentl105@nycstudents.net>
2023-10-26 10:57:39 -0400

    a
    Co-authored-by: Henryw48 <Henryw48@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-
+const images {
+    i1 = document.getElementById("image1");
+    i2 = document.getElementById("image2");
+    i3 = document.getElementById("image3");
+    i4 = document.getElementById("image4");
+    i5 = document.getElementById("image5");
+    i6 = document.getElementById("image6");
+    i7 = document.getElementById("image7");
+    i8 = document.getElementById("image8");
+    i9 = document.getElementById("image9");
+    i10 = document.getElementById("image10");
+    i11= document.getElementById("image11");
+    i12 = document.getElementById("image12");
+    i13 = document.getElementById("image13");
+    i14 = document.getElementById("image14");
+    i15 = document.getElementById("image15");
+    i16= document.getElementById("image16");
+}
 
 for (i = 1; i <= 16; i++) {
-    document.getElementById("image" + i).addEventListener('click', function{
+    document.getElementById("image" + i).addEventListener('click', function {
         flip("image" + i);
     })
 }
+
+
+let select = first;
+
+function randomizeCards() {
+
+}
+function flipcard() {
+}
+
+function match(){
+
+}
+
+function removeCards(){
+
+}
+
+
